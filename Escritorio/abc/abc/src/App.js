0179b49661d85633319136c2e9f0fd6d0050ebf1
Gabi <gabrielcorrieri1@gmail.com>
2022-02-07 01:29:18 -0300

    primer-cambio

---
+++ Escritorio/abc/abc/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+<div>
+  <header>
+    <div className='header-container'>
+  <h1>Tienda de ropa</h1>
+    <Menu/>
+    </div>
+  </header>
+</div>
+  );
+}
+
+export default App;
---
+++ Escritorio/abc/abc/src/components/Menu.js
@@ -0,0 +1,18 @@
+import './Menu.css';
+function Menu(){
+    return (
+            <ul>
+                <li>
+                    Productos
+                </li>
+                <li>
+                    Nosotros
+                </li>
+                <li>
+                    Contacto
+                </li>
+            </ul>
+    );
+}
+
+export default Menu;
